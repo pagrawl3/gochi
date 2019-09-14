c78bbfa63bb0cae879682f7d06566f8091ead5c0
iamsimranjot <iamsimranjot@gmail.com>
2019-09-15 04:11:54 +0530

    TableItem

--- frontend/src/components/CategoryDot/CategoryDot.js
+++ frontend/src/components/CategoryDot/CategoryDot.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './CategoryDot.scss';
 
-function CategoryDot({ color, size = 12, className, active }) {
+function CategoryDot({ color, size = 12, className, active, selected, onClick }) {
   const activeClassName = active ? 'active' : '';
   return (
     <div
       style={{ backgroundColor: color, width: size, height: size }}
       className={`category-dot ${activeClassName} ${className}`}
+      onClick={onClick}
     />
   );
 }
--- frontend/src/containers/Dashboard/Table/components/TableItem/TableItem.js
+++ frontend/src/containers/Dashboard/Table/components/TableItem/TableItem.js
@@ -1,22 +1,26 @@
 import React from "react";
 import Dayjs from "dayjs";
 
-import CheckBox from '../../../../../components/Checkbox';
-import CategoryDot from '../../../../../components/CategoryDot';
+import CheckBox from "../../../../../components/Checkbox";
+import CategoryDot from "../../../../../components/CategoryDot";
 
 import "./TableItem.scss";
 import Context from "../../../../../components/Context";
 
 function TableItem({ title, subject, senderName, categoryId, status, date }) {
-  const { categories } = React.useContext(Context);
+  const { setState, categories } = React.useContext(Context);
 
-  const category = categories.find(category => category._id === categoryId) || {};
+  const category =
+    categories.find(category => category._id === categoryId) || {};
   const { color, duration } = category;
 
   const currentDate = Dayjs();
   const timeElapsed = currentDate.diff(date, "second");
   const timeRemaining = (duration - timeElapsed) / 60;
   var timeRemainingText = "";
+
+  var categoryClicked = false;
+  const activeClassName = categoryClicked ? "active" : "";
 
   if (timeRemaining < 1 && timeRemaining > 0) {
     timeRemainingText = "One Minutes Remaining";
@@ -29,11 +33,24 @@ function TableItem({ title, subject, senderName, categoryId, status, date }) {
   return (
     <div className="tableItem">
       <div className="tableItem-column lhs">
-        <CheckBox className="tableItem-column-accessory"/>
+        <CheckBox className="tableItem-column-accessory" />
         <div className="tableItem-column-labelsContainer">
           <div className="tableItem-column-labelsContainer-titleContainer">
             {/* Category Dot */}
-            <CategoryDot color={category.color} className="tableItem-column-labelsContainer-titleContainer-category"/>
+            <CategoryDot
+              color={category.color}
+              className="tableItem-column-labelsContainer-titleContainer-category"
+              onClick={() => setState({ categoryClicked: !categoryClicked })}
+            />
+            <div
+              className={`tableItem-column-labelsContainer-titleContainer-categoryDots ${activeClassName}`}>
+              {categories.map((categoryItem, i) => (
+                <CategoryDot
+                  selected={categoryClicked}
+                  color={categoryItem.color}
+                />
+              ))}
+            </div>
             {/* Email Title */}
             <div className="tableItem-column-labelsContainer-titleContainer-title">
               {title}
