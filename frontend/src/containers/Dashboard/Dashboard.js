d8ce2e3d43f5510cfa12acd5326e189af511495a
iamsimranjot <iamsimranjot@gmail.com>
2019-09-14 12:59:33 +0530

    Initial Setup

---
+++ frontend/src/containers/Dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Dashboard.scss';
+
+function Dashboard () {
+    return (
+        <div className='hello-world'>Hello World !!</div>
+    )
+}
+
+export default Dashboard;
