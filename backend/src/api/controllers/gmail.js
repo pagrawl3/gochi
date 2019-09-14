809c9901998c16c22383fa762a5aeb340fbe47bb
Kartik Grewal <kartik.grewal@gmail.com>
2019-09-15 00:19:22 +0530

    Changed backend endpoint

--- backend/src/api/controllers/gmail.js
+++ backend/src/api/controllers/gmail.js
@@ -1,69 +1,78 @@
-const axios = require('axios');
-const qs = require('query-string');
-const jwt = require('jsonwebtoken');
-const User = require('../../models/user');
-const CONST = require('../../const');
-const utils = require('../../utils');
+const axios = require("axios");
+const qs = require("query-string");
+const jwt = require("jsonwebtoken");
+const User = require("../../models/user");
+const CONST = require("../../const");
+const utils = require("../../utils");
 
 const { returnError, returnSuccess } = utils;
 
 exports.getLoginURL = async function(req, res) {
   const URL = `${CONST.GMAIL.AUTH_URI}?client_id=${CONST.GMAIL.CLIENT_ID}&response_type=${CONST.GMAIL.RESPONSE_TYPE}&redirect_uri=${CONST.GMAIL.REDIRECT_URI}&scope=${CONST.GMAIL.SCOPE}&access_type=${CONST.GMAIL.ACCESS_TYPE}&prompt=consent`;
-  returnSuccess(res, 'Link generated successfully', { URL: URL });
+  returnSuccess(res, "Link generated successfully", { URL: URL });
 };
 
 exports.getAccessToken = async function(req, res) {
-  let code = req.body.code ? req.body.code : '';
+  let code = req.body.code ? req.body.code : "";
 
-  if (code) {
-    const CONFIG = {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    };
-
-    const requestBody = {
-      code: code,
-      client_id: CONST.GMAIL.CLIENT_ID,
-      client_secret: CONST.GMAIL.CLIENT_SECRET,
-      grant_type: CONST.GMAIL.GRANT_TYPE,
-      redirect_uri: CONST.GMAIL.REDIRECT_URI
-    };
-
-    axios
-      .post(`${CONST.GMAIL.TOKEN_URI}`, qs.stringify(requestBody), CONFIG)
-      .then(res => {
-        if (res.data.access_token) {
-          let decoded = jwt.decode(res.data.id_token);
-          let { email } = decoded;
-          User.findOne({ email })
-            .then(user => {
-              if (!user) {
-                let newUser = {
-                  email: email,
-                  auth: res.data.access_token,
-                  refreshToken: res.data.refresh_token
-                };
-                new User(newUser)
-                  .save()
-                  .then(newUser =>
-                    axios.get('https://www.googleapis.com/gmail/v1/users/me/threads', CONFIG).then(res => {
-                      console.log(res);
-                    })
-                  )
-                  .catch(err => returnError(res, err));
-              }
-            })
-            .catch(e => console.log('rror', e));
-        }
-        // User.findOne({ email })
-        //   .then(user => returnSuccess(res, "Logged in successfully", user))
-        //   .catch(e => returnError(res, "User does not exist"));
-      })
-      .catch(err => {
-        console.log('err', err);
-      });
+  if (!code) {
+    returnError(res, "Code not provided");
   }
 
-  returnSuccess(res, '');
+  const CONFIG = {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  };
+
+  const requestBody = {
+    code: code,
+    client_id: CONST.GMAIL.CLIENT_ID,
+    client_secret: CONST.GMAIL.CLIENT_SECRET,
+    grant_type: CONST.GMAIL.GRANT_TYPE,
+    redirect_uri: CONST.GMAIL.REDIRECT_URI
+  };
+
+  await axios
+    .post(`${CONST.GMAIL.TOKEN_URI}`, qs.stringify(requestBody), CONFIG)
+    .then(async res => {
+      if (res.data.access_token) {
+        let decoded = jwt.decode(res.data.id_token);
+        let { email } = decoded;
+        await User.findOne({ email })
+          .then(async user => {
+            if (!user) {
+              let newUser = {
+                email: email,
+                auth: res.data.access_token,
+                refreshToken: res.data.refresh_token
+              };
+              await new User(newUser)
+                .save()
+                .then(newUser => {
+                  console.log("newUser", newUser);
+                })
+                .catch(err => returnError(res, err));
+            }
+          })
+          .catch(e => console.log("rror", e));
+      }
+      // User.findOne({ email })
+      //   .then(user => returnSuccess(res, "Logged in successfully", user))
+      //   .catch(e => returnError(res, "User does not exist"));
+    })
+    .catch(err => {
+      console.log("err", err);
+    });
 };
+
+function getThreads() {
+  axios
+    .get("https://www.googleapis.com/gmail/v1/users/me/threads", CONFIG)
+    .then(res => {
+      console.log(res);
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
