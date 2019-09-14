db8eeff9553570adf358ded5b95cfe6e1749b317
iamsimranjot <iamsimranjot@gmail.com>
2019-09-14 22:11:43 +0530

    Table Item Completed

--- frontend/src/containers/Dashboard/Table/Table.js
+++ frontend/src/containers/Dashboard/Table/Table.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Table.scss';
 
+import './components/TableItem';
+import TableItem from './components/TableItem';
+
 function Table () {
     return (
         <div className='table'>
-
+            <TableItem title='Feature Request <> Reassign Chats as feature in permissions' subject="Reassign chat on 'My Chats' page is currently available to all permission groups with access to Athena Chat. However, we have received a request…" senderName='Suraj' status='First response needed' timeremaining='8 Minutes remaining'/>
         </div>
     )
 }
